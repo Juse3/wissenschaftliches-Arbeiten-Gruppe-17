8182c99753be618bc4c13610fbb1f9c347513815
Judith Bose <judith.bose@tu-dortmund.de>
2022-12-21 11:14:31 +0100

    Update Aufgabe1.R
    - wahrscheinlich ist es sinnvoll einen seed zu setzen, um die Ergebnisse reproduzierbar zu machen
    - es macht denke ich mehr Sinn, das Alter immer abzurunden (deswegen die Funktion floor)
    - für Interesse an Mathematik habe ich keinen richtigen Zusammenhang festgelegt (also ich habe lediglich Wahrscheinlichkeiten festgelegt, wie oft welches Niveau vorkommt). Da allen Studiengängen mathelastig sind, sind die Unterschiede vermutlich nicht so stark und nicht unbedingt vom Studiengang abhängig.

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -1,8 +1,12 @@
 ## Aufgabe 1
+set.seed(17)
 
 ID <- 1:100
 
-Alter <- round(rnorm(n = 100, mean = 25, sd = 2))
+Alter <- floor(rnorm(n = 100, mean = 25, sd = 2))
 
 Studienfach <- sample(x = c("Statistik", "Data Science", "Mathe", "Informatik"), 
                       size = 100, replace = TRUE, prob = c(30, 30, 15, 25))
+
+InteresseAnMathematik <- sample(c(1:7), size = 100, replace = TRUE, 
+                                prob = c(5, 5, 10, 15, 15, 25, 25))
