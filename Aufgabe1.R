c08742bc14241d67e1fbf01b0218ea347a9d3795
Judith Bose <judith.bose@tu-dortmund.de>
2022-12-23 13:31:25 +0100

    MathematikLK
    - ich hab bei "Interesse am Programmieren" einen anderen Zusammenhang hinzugefügt (jetzt ist eine negative Korrelation zwischen Interesse an Mathematik vorhanden)
    - die Interessezahlen ergeben in der Addition also 8 (ich hoffe das war verständlich ausgedrückt, sonst kannst du nochmal nachfragen)
    - ich halte den Zusammenhang eigentlich für ganz sinnvoll und dann haben wir wirklich einen direkten Zusammenhang mit einer anderen Variable
    - die Variable MathematikLK habe ich hinzugefügt. Aktuell besteht der Zusammenhang nur mit Studienfach und Interesse an Mathematik. Hältst du es für sinnvoll noch das Interesse am Programmieren mit einzubeziehen?
    - außerdem habe ich die Daten noch in einen Dataframe zusammengefügt

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -13,3 +13,32 @@ InteresseAnMathematik <- sample(c(1:7), size = 100, replace = TRUE,
 
 InteresseAnProgrammieren <- sample(x = 1:7, size = 100, replace = TRUE,
                                   prob = c(10, 10, 5, 5, 15, 15, 40))
+
+IP <- as.numeric(as.character(factor(InteresseAnMathematik, levels = c(1:7), labels = c(7:1))))
+
+MatheLK <- function(Studienfach, InteresseAnMathematik){
+  i <- 1
+  MathematikLK <- character(length(Studienfach))
+  while (i < length(Studienfach) + 1){
+    if (Studienfach[i] == "Mathematik" & InteresseAnMathematik[i] >= 5){
+      MathematikLK[i] <- "Ja"
+    }
+    if (Studienfach[i] == "Statistik" & InteresseAnMathematik[i] >= 4){
+      MathematikLK[i] <- "Ja"
+    }
+    if ((Studienfach[i] == "Informatik" | Studienfach[i] == "Data Science") & 
+        InteresseAnMathematik[i] >= 3){
+      MathematikLK[i] <- "Ja" 
+    }
+    else{
+      MathematikLK[i] <- "Nein"
+    }
+    i <- i + 1
+  }
+  
+  return(MathematikLK)
+}
+
+MathematikLK <- MatheLK(Studienfach, InteresseAnMathematik)
+daten <- data.frame(ID, Alter, Studienfach, InteresseAnMathematik, 
+                    IP, MathematikLK)
