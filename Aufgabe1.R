96c7138048a71479430ac5e5fed1009824029cf1
larissaaaaaaaa <larissa.schoeneich@tu-dortmund.de>
2022-12-25 01:46:40 +0100

    csv-Datei
    Ich verstehe deinen Ansatz bei Interesse an Programmieren. Damit da am Ende was Sinnvolles rauskommt, habe ich die Wahrscheinlichkeiten bei Interesse an Mathematik noch ein bisschen verändert.
    Ich finde deine Kriterien für Mathe LK sinnvoll, ich hab nur Interesse an Mathematik beim Mathestudium um 1 runtergesetzt. Ich finde aber nicht, dass wir Programmieren da noch einfließen lassen sollten, weil es bereits von zwei sinnvollen Größen abhängt.
    Des Weiteren habe ich schon mal versucht, eine csv-Datei zu erstellen, allerdings kommt da dann noch so eine namenslose erste Spalte dazu, die inhaltlich unsere ID abbildet. Vielleicht weißt du ja, wie man die vermeidet oder sie benennt, dann könnte das unsere ID werden.

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -1,4 +1,5 @@
 ## Aufgabe 1
+
 set.seed(17)
 
 ID <- 1:100
@@ -9,7 +10,7 @@ Studienfach <- sample(x = c("Statistik", "Data Science", "Mathe", "Informatik"),
                       size = 100, replace = TRUE, prob = c(30, 30, 15, 25))
 
 InteresseAnMathematik <- sample(c(1:7), size = 100, replace = TRUE, 
-                                prob = c(5, 5, 10, 15, 15, 25, 25))
+                                prob = c(10, 15, 15, 10, 15, 20, 15))
 
 InteresseAnProgrammieren <- as.numeric(as.character(
   factor(InteresseAnMathematik, levels = c(1:7), labels = c(7:1))))
@@ -18,7 +19,7 @@ MatheLK <- function(Studienfach, InteresseAnMathematik){
   i <- 1
   MathematikLK <- character(length(Studienfach))
   while (i < length(Studienfach) + 1){
-    if (Studienfach[i] == "Mathematik" & InteresseAnMathematik[i] >= 5){
+    if (Studienfach[i] == "Mathematik" & InteresseAnMathematik[i] >= 4){
       MathematikLK[i] <- "Ja"
     }
     if (Studienfach[i] == "Statistik" & InteresseAnMathematik[i] >= 4){
@@ -38,5 +39,8 @@ MatheLK <- function(Studienfach, InteresseAnMathematik){
 }
 
 MathematikLK <- MatheLK(Studienfach, InteresseAnMathematik)
+
 daten <- data.frame(ID, Alter, Studienfach, InteresseAnMathematik, 
                     InteresseAnProgrammieren, MathematikLK)
+
+write.csv(daten, "C:/Users/laris/Desktop/wissenschaftliches-Arbeiten-Gruppe-17/daten.csv")
