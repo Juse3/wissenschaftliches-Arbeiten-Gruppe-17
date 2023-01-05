2e4ecad9ed80bced7771a00868f1656764bacb0a
KevinUnrau <119624856+KevinUnrau@users.noreply.github.com>
2023-01-05 08:08:36 +0100

    Update Funkionen-R-Skript 2.R
    Funktionen für Kurtosis und Schiefe ergänzt

--- Funkionen-R-Skript 2.R
+++ Funkionen-R-Skript 2.R
@@ -12,3 +12,27 @@ mode <- function(vec){
   u[tab == max(tab)]
 }
 #Funktion um den Modus zu bestimmen
+
+#Funktionen um Schiefe und Kurtosis zu bestimmen:
+
+skewness <- function(vec){
+  
+  length <- length(vec)
+  mean <- mean(vec)
+  sd <- sd(vec)
+  
+  result <- (1/length)*sum(((vec-mean)/sd)^3)
+
+  return(result)
+}
+
+kurtosis <- function(vec){
+  
+  length <- length(vec)
+  mean <- mean(vec)
+  sd <- sd(vec)
+  
+  result <- (1/length)*sum(((vec-mean)/sd)^4)
+  
+  return(result)
+}
