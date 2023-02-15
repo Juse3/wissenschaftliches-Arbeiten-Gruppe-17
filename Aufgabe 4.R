d1902830d8d500e32253eaa01559631267b6b76e
KevinUnrau <119624856+KevinUnrau@users.noreply.github.com>
2023-02-15 08:31:40 +0100

    Update Aufgabe 4.R
    -Überblick über die Variablen MatheLK und Studiengang hinzugefügt
    -Zusammenhang zwischen Interesse an Mathematik und Mathe LK hinzugefügt

--- Aufgabe 4.R
+++ Aufgabe 4.R
@@ -3,13 +3,13 @@
 #Daten einlesen
 daten <- read.csv("daten.csv", header = TRUE, sep = ";")
 
-#Überblick über vorhandene Variablen
+#Ueberblick ueber vorhandene Variablen
 summary(daten)
 
+#Ueberblick über alle Daten
 zusatz()
-#Überblick über alle Daten
 
-#Überblick über Variable Alter
+#Ueberblick ueber Variable Alter
 descr_metric(daten$Alter)
 #Mittelwert liegt bei 24.51, Median und Modus liegen bei 25
 #Standardabweichung liegt bei 2.11
@@ -29,7 +29,7 @@ descr_metric(daten[daten$Studienfach == "Mathe",4])
 descr_metric(daten[daten$Studienfach == "Statistik",4])
 #Mittelwert = 4.536
 
-#Es wirkt nicht so als würde ein Zusammenhang zwischen Studiengang und Interesse
+#Es wirkt nicht so als wuerde ein Zusammenhang zwischen Studiengang und Interesse
 #an Mathematik bestehen, da die Mathematiker den geringsten Mittelwert aufweisen
 
 #Interesse an Programmieren, aufgeteilt nach Studiengang
@@ -46,16 +46,34 @@ descr_metric(daten[daten$Studienfach == "Mathe",5])
 descr_metric(daten[daten$Studienfach == "Statistik",5])
 #Mittelwert = 3.464
 
-#Es wirkt auch nicht so als würde ein Zusammenhang zwischen Studiengang und Interesse
+#Es wirkt auch nicht so als wuerde ein Zusammenhang zwischen Studiengang und Interesse
 #am Programmieren bestehen, da die Informatiker einen deutlich kleineren Mittelwert
 #als zum Beispiel die Mathematiker haben
 
+#Ueberblick ueber Variable Studienfach
+descr_cat(data$Studienfach)
+#Die meisten Studierenden aus dem Datensatz studieren Data Science (32%)
+#Es folgt Statistik (28%)
+#Informatik(23%), Mathe(17%)
+
+#Ueberblick ueber Variable MatheLK
+descr_cat(data$MatheLK)
+#Fast die Haelfte war im MatheLK(45%)
 
 relat_cat(daten$MatheLK,daten$Studienfach,"MatheLK","Studienfach")
 #Es besteht ein Zummmenhang zwischen MatheLK und und dem Studienfach, nur Studenten
 #mit Fach Data Science und Informatik hatten MatheLK
 
-
 bivar_dichotom(daten$Alter,daten$MatheLK,"Ja","Alter","MatheLK")
 #Es besteht kein Zusammenhang zwischen Alter und MatheLK, da nur geringe
 #negative Korrelation bzw. Kovarianz festgestellt werden kann.
+
+#Zusammenhang zwischen Interesse an Mathematik und Mathe LK
+bivar_dichotom(daten$InteresseAnMathematik, daten$MatheLK, "Ja",
+               "Interesse an Mathematik", "MatheLK")
+#Es besteht ein leicht positiver Zusammenhang zwischen MatheLK und Interesse an Mathematik.
+#Leute mit einem sehr geringen Interesse an Mathematik (Interesse = 1 oder 2)
+#waren nicht im MatheLK. Das sind insgesamt 21 Studierende. Ab einem Interesse
+#von 3 gibt es immer mindestens so viele Studierende, die im LK waren wie Studierende,
+#die nicht im Mathe-LK waren.
+#Korrelationskoeffizient nach Pearson: 0.3220041
